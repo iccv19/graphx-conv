d79dcbb9d76bb20bc6cc31385ca3117d49a1a22c
mingjie.xiao <372306102@qq.com>
2019-11-05 15:19:47 +0800

    Fixed arguments bugs
    Fixed "'PointcloudDeformNet' object has no attribute 'optim' " and some arguments order bugs.

--- src/networks.py
+++ src/networks.py
@@ -279,7 +279,9 @@ class PointcloudDeformNet(nnt.Net, nnt.Module):
                              adain=adain, projection=projection)
         self.pc = pc_dec(self.pc_enc.output_shape, activation=activation)
 
-        self.optim['optimizer'] = T.optim.Adam(self.trainable, 1e-4, weight_decay=0) if optimizer is None \
+        self.optim = {}
+
+        self.optim['optimizer'] = T.optim.Adam(self.trainable, 1e-4, weight_decay=0) if optimizer is None
             else optimizer(self.trainable)
         self.optim['scheduler'] = scheduler(self.optim['optimizer']) if scheduler else None
         self.kwargs = kwargs
@@ -313,13 +315,13 @@ class PointcloudDeformNet(nnt.Net, nnt.Module):
         self.stats['eval']['scalars']['eval_chamfer'] = nnt.utils.to_numpy(loss)
         del loss
 
-    def learn(self, init_pc, input, gt_pc, *args, **kwargs):
+    def learn(self, optim, init_pc, input, gt_pc, *args, **kwargs):
         self.train(True)
-        self.optim['optimizer'].zero_grad()
+        optim['optimizer'].zero_grad()
         loss = self.train_procedure(init_pc, input, gt_pc, reduce='mean')
         if not (T.isnan(loss) or T.isinf(loss)):
             loss.backward()
-            self.optim['optimizer'].step()
+            optim['optimizer'].step()
 
         self.stats['train']['scalars']['chamfer'] = nnt.utils.to_numpy(loss)
         del loss
